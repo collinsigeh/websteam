fbd19f49fb5a2a0097faf3676a4e16dab1d7004a
Collins Igeh <igehac@gmail.com>
2023-05-30 09:03:33 +0100

    confirmed the size of top-of-page-ad and hero image

--- resources/views/newhome.blade.php
+++ resources/views/newhome.blade.php
@@ -4,57 +4,55 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- Favicons -->
+    @include('layouts.favicon')
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="custom_assets/web_style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
 <body>
-    <pre>
-
-
-
-
-
-
-
-
-
-    </pre>
+    <div id="pre-header-ad">
+        
+    </div>
     <header>
-        <a href="#" class="logo">PENGlobal</a>
+        <a href="{{ route('welcome')}}" class="logo">PENGlobal</a>
 
         <input type="checkbox" name="" id="menu-bar">
-        <label for="menu-bar">Menu</label>
+        <label for="menu-bar"><i class="fas fa-bars"></i></label>
 
         <nav class="navbar">
             <ul>
-                <li><a href="#">home</a></li>
-                <li><a href="#">about</a></li>
-                <li><a href="#">pages +</a>
+                <li><a href="{{ route('welcome') }}">home</a></li>
+                <li><a href="{{ route('about') }}">about</a></li>
+                <li><a href="#">segments +</a>
                     <ul>
                         <li><a href="#">Blogs</a></li>
                         <li><a href="#">Services +</a>
-                            <ul>
-                                <li><a href="#">HTML</a></li>
-                                <li><a href="#">CSS</a></li>
-                                <li><a href="#">JavaScript</a></li>
-                            </ul>
                         </li>
                     </ul>
                 </li>
-                <li><a href="#">reviews</a></li>
-                <li><a href="#">gallery +</a>
-                    <ul>
-                        <li><a href="#">grid gallery</a></li>
-                        <li><a href="#">flex gallery</a></li>
-                    </ul>
-                </li>
-                <li><a href="#">contact</a></li>
+                <li><a href="#">donate</a></li>
+                <li><a href="{{ route('contact') }}">contact</a></li>
             </ul>
         </nav>
     </header>
 
     <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-9">
+                    <div id="latest-post">
+
+                    </div>
+                </div>
+            </div>
+        </div>
         <pre>
 
 
