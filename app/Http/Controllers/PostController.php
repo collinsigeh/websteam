3a2109f83f3833334191bdfb528f4ac05803078d
Collins Igeh <igehac@gmail.com>
2023-06-26 21:18:50 +0100

    modified PostController to allow for the display of videos in backend

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -156,6 +156,9 @@ class PostController extends Controller
 
         $categories = Category::orderBy('category_name', 'asc')->get();
 
+        $post->body = str_replace('<figure class="media"><oembed url="https://www.youtube.com/watch?v=', '<iframe width="480" height="270" src="https://www.youtube.com/embed/', $post->body);
+        $post->body = str_replace('></oembed></figure>', ' frameborder="0" allowfullscreen></iframe>', $post->body);
+
         return view('posts.show', [
             'post' => $post,
             'categories' => $categories,
